59b485ac00c36cd2d67c232ed86ffa57d18a007c
junyeong <gowjr207@gmail.com>
2024-10-14 16:41:26 +0900

    test: h2 save find test

--- src/test/java/com/auff/user/MemberServiceTest.java
+++ src/test/java/com/auff/user/MemberServiceTest.java
@@ -3,31 +3,26 @@ package com.auff.user;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
-class UserServiceTest {
+class MemberServiceTest {
 
   @Autowired
-  private UserService userService;
+  private MemberService memberService;
 
   @Autowired
-  private UserRepository userRepository;
+  private MemberRepository memberRepository;
 
   @Test
   void 생성_후_조회() {
-    long id = userService.create();
+    long id = memberService.create();
 
-    User user = userService.find(id);
+    Member member = memberService.find(id);
 
-    assertEquals(id, user.getId());
+    assertEquals(id, member.getId());
   }
 }
